543955c8952f69d2fd8eba18b6a2183e4458733d
Abraham Kim <tkim25@kennesaw.edu>
2018-10-29 11:38:15 -0400

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Router, Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { NotesComponent } from './notes/notes.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'notes', component: NotesComponent },
+  { path: 'feedback', component: FeedbackComponent },
+  { path: '', component: NotesComponent, pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent },
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FeedbackComponent,
+    NotesComponent,
+    NotFoundComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: true}),
   ],
   providers: [],
   bootstrap: [AppComponent]
